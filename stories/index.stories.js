632597d2b99a1277e8fee1c2b0406f371eea329c
Naoki Kobayashi <naokiod@gmail.com>
2023-06-15 17:56:57 +0900

    Enable to load with partial handlebars

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,8 +1,11 @@
 import Handlebars from "handlebars";
 import source from "./index.handlebars";
+import subtitle from "./subtitle.handlebars";
+
+Handlebars.registerPartial("subtitle", subtitle);
 
 const template = Handlebars.compile(source);
-const html = template({ title: "Test" });
+const html = template({ title: "Test", subtitle: "Sub" });
 
 export default { title: "index" };
 export const Preview = () => html;
